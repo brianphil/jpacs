20155547a3d674184f34001f0cab85f26e2a33bc
brianphil <brianphilemon@gmail.com>
2024-09-05 07:47:53 +0300

    added routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,25 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const { isAuthenticated , isEditor} = require('../middlewares/auth'); // Import isAuthenticated middleware
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { isAuthenticated, isEditor } = require("../middlewares/auth"); // Import isAuthenticated middleware
 
 // Register
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password, role, firstName, lastName, email, affiliation, bio } = req.body;
   try {
     let user = await User.findOne({ username });
-    if (user) return res.status(400).json({ message: 'Username already exists' });
+    if (user) return res.status(400).json({ message: "Username already exists" });
 
     user = await User.findOne({ email });
-    if (user) return res.status(400).json({ message: 'Email already exists' });
-
-    user = new User({ username, password, role, firstName, lastName, email, affiliation, bio });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+    const isApproved = role == "author" ? true : false;
+    user = new User({
+      username,
+      password,
+      role,
+      firstName,
+      lastName,
+      email,
+      affiliation,
+      bio,
+      isApproved,
+    });
     await user.save();
 
     const payload = { id: user.id };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
 
     res.status(201).json({ token });
   } catch (error) {
@@ -28,7 +38,7 @@ router.post('/register', async (req, res) => {
 });
 
 // Login Route
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
@@ -40,41 +50,41 @@ router.post('/login', async (req, res) => {
       if (isMatch) {
         // Generate JWT
         const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, {
-          expiresIn: '1h',
+          expiresIn: "1h",
         });
 
         // Respond with token and user details
         res.json({ token, user });
       } else {
-        return res.status(401).json({ message: 'Invalid credentials' });
+        return res.status(401).json({ message: "Invalid credentials" });
       }
     } else {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
 // Get current user
-router.get('/user', isAuthenticated, async (req, res) => {
+router.get("/user", isAuthenticated, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password');
+    const user = await User.findById(req.user.id).select("-password");
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
     res.json(user);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
 // Logout route
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   // Simply respond with a success message, the client should handle the token removal
-  res.json({ message: 'Logged out successfully' });
+  res.json({ message: "Logged out successfully" });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,50 +1,49 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-require('dotenv').config();
-const morgan = require('morgan'); // Import morgan
-const multer = require('multer'); // Import multer
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config();
+const morgan = require("morgan"); // Import morgan
+const multer = require("multer"); // Import multer
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 // Serve static files from the "uploads" directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Multer setup
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'server/uploads/'); // Set your desired upload directory
+    cb(null, "server/uploads/"); // Set your desired upload directory
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname); // Naming the uploaded file
+    cb(null, Date.now() + "-" + file.originalname); // Naming the uploaded file
   },
 });
 const upload = multer({ storage: storage });
 
 // Routes
-const authRoutes = require('./routes/auth');
-const reviewRoutes = require('./routes/reviews');
-const articleRoutes = require('./routes/articles'); // No need to pass upload here
-const reviewerRoutes = require('./routes/reviewers');
-app.use('/api/auth', authRoutes);
-app.use('/api/articles', articleRoutes(upload)); // Pass upload here
-app.use('/api/reviews', reviewRoutes);
-app.use('/api', reviewerRoutes);
+const authRoutes = require("./routes/auth");
+const reviewRoutes = require("./routes/reviews");
+const articleRoutes = require("./routes/articles"); // No need to pass upload here
+const reviewerRoutes = require("./routes/reviewers");
+app.use("/api/auth", authRoutes);
+app.use("/api/articles", articleRoutes(upload)); // Pass upload here
+app.use("/api/reviews", reviewRoutes);
+app.use("/api", reviewerRoutes);
 
-app.get('*', (req, res) => {
-    res.status(404).json({ error: 'not found!' });
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "not found!" });
 });
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
