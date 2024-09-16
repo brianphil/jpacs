1c9cd9613a91dc81990a7dee93a2d5aa39d36c7e
brianphil <brianphilemon@gmail.com>
2024-09-16 17:42:38 +0300

    Reverting to updating fe

--- fe/src/services/config.js
+++ fe/src/services/config.js
@@ -1,4 +1,5 @@
 export const config = {
     // BASE_URL: 'http://localhost:5000'
-    BASE_URL: 'https://portal.jpacsciences.com'
+    // BASE_URL: 'https://portal.jpacsciences.com'
+    BASE_URL: 'https://node-webscrapper.onrender.com'
 } 
