83b09aed7a22e9be67c9466d11c7f98c7979a1f6
brianphil <brianphilemon@gmail.com>
2024-09-17 11:15:18 +0300

    reconnect mongo

--- server/server.js
+++ server/server.js
@@ -6,29 +6,30 @@ require("dotenv").config();
 const morgan = require("morgan"); // Import morgan
 const multer = require("multer"); // Import multer
 const app = express();
+const mongoose = require("mongoose")
 // const sequelize = require('./db');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://jpacs:Jpaks.@cluster0.somakh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://jpacs:Jpaks.@cluster0.somakh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
 
 // sequelize.sync({ alter: true })
 //   .then(() => {
@@ -46,10 +47,10 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 // MongoDB Connection
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Multer setup
 const storage = multer.diskStorage({
